5b868b589ca8bed54df2eba1264c1be711720275
Kent Watsen <kent@watsen.net>
2023-04-18 16:39:51 -0400

    Make userlinks not show user's internal name

--- inc/common.php
+++ inc/common.php
@@ -1659,8 +1659,8 @@ function userlink($username = null, $textonly = false) {
         if($textonly){
             $data['name'] = $INFO['userinfo']['name']. ' (' . $INPUT->server->str('REMOTE_USER') . ')';
         }else {
-            $data['name'] = '<bdi>' . hsc($INFO['userinfo']['name']) . '</bdi> '.
-                '(<bdi>' . hsc($INPUT->server->str('REMOTE_USER')) . '</bdi>)';
+            $data['name'] = '<bdi>' . hsc($INFO['userinfo']['name']) . '</bdi> '; /*.
+                '(<bdi>' . hsc($INPUT->server->str('REMOTE_USER')) . '</bdi>)';*/
         }
     }
 
